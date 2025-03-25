d98a3a4d498c52f03a00ac696258371977fccc62
Phuwich <phuwich.27.chonthong+github@gmail.com>
2025-03-25 12:35:32 +0700

    Add files via upload

---
+++ cell03/ex02/calc.js
@@ -0,0 +1,50 @@
+let output;
+
+function calc() {
+    let input1 = Number(document.getElementById('input1').value);
+    let input2 = Number(document.getElementById('input2').value); 
+    let operator = String(document.getElementById('operator').value);
+
+    if ((isNaN(input1) || isNaN(input2))) {
+        alert("error :(");
+    }
+    if (input1 < 0 || input2 < 0) {
+        alert("error :(");
+    }
+
+    if (operator == '+') {
+        output = input1 + input2;
+    }
+    if (operator == '-') {
+        output = input1 - input2;
+    }
+    if (operator == '*') {
+        output = input1 * input2;
+    }
+    //devide by 0
+    if (operator == '/') {
+        if (input2 == 0) {
+            alert("It's over 9000!");
+        } else {
+            output = input1 / input2;
+        }
+    }
+    if (operator == '%') {
+        if (input2 == 0) {
+            alert("It's over 9000!");
+        } else {
+            output = input1 % input2;
+        }
+    }
+
+    // clear input
+    console.log(output);
+    document.getElementById('input1').value = '';
+    document.getElementById('input2').value = '';
+}
+
+function showAlert() {
+    alert('Please, use me...');
+}
+
+setInterval(showAlert, 30000);
